3d1e59a028a9918a2b8cc6cae4e744dafd0d59a8
ADITYA KUMAR <adityak@sahusoft.com>
2017-04-20 16:13:07 +0530

    pull.sh

--- webhook-deploy/auto.php
+++ webhook-deploy/auto.php
@@ -20,9 +20,8 @@
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8">
-	<title>GIT DEPLOYMENT SCRIPT</title>
 </head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<body>
 
 <?php echo $tmp; ?>
 </pre>
