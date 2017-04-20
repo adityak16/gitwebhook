99db79c6dc8bdc887a43df9aa96dea985ae7f9ee
ADITYA KUMAR <adityak@sahusoft.com>
2017-04-20 15:57:11 +0530

    pull.sh

--- _book/webhook-deploy/auto.php
+++ _book/webhook-deploy/auto.php
@@ -1,11 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
  // Prevent accidental XSS
  header("Content-type: text/plain"); 
  // Run the script - make sure that your SSH key is set with *no* password
   if ( $_POST['payload'] ) {
   $tmp = shell_exec("./pull.sh"); 
-  echo "<td>".$tmp."</td>";
+  echo "<h1>".$tmp."</h1>";
   // $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
   // $output .= htmlentities(trim($tmp)) . "\n";
  }
 ?>
+</body>
+</html>
