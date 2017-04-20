90e7274092c64af7c559fc9cf7252502e878543c
ADITYA KUMAR <adityak@sahusoft.com>
2017-04-20 15:30:37 +0530

    pull.sh

--- webhook-deploy/auto.php
+++ webhook-deploy/auto.php
@@ -2,15 +2,16 @@
  // Prevent accidental XSS
  // header("Content-type: application/json"); 
  // Run the script - make sure that your SSH key is set with *no* password
- if ( $_POST['payload'] ) {
+ // if ( $_POST['payload'] ) {
   $tmp = shell_exec("./pull.sh"); 
   $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
   $output .= htmlentities(trim($tmp)) . "\n";
-}
+
 ?>
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 	<meta charset="UTF-8">
 	<title>GIT DEPLOYMENT SCRIPT</title>
 </head>
