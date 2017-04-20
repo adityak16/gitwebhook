6e6aad07841924cc0cc73669574f5878c1004b8a
ADITYA KUMAR <adityak@sahusoft.com>
2017-04-20 15:14:27 +0530

    pull.sh

--- webhook-deploy/auto.php
+++ webhook-deploy/auto.php
@@ -1,6 +1,6 @@
 <?php
  // Prevent accidental XSS
- header("Content-type: text/plain"); 
+ header("Content-type: application/json"); 
  // Run the script - make sure that your SSH key is set with *no* password
  if ( $_POST['payload'] ) {
   shell_exec("./pull.sh"); 
