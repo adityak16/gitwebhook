c692ff0ab8fcaac31d674608210987fe484447c2
ADITYA KUMAR <adityak@sahusoft.com>
2017-04-20 16:25:04 +0530

    git

--- _book/webhook-deploy/auto.php
+++ _book/webhook-deploy/auto.php
@@ -1,29 +1,11 @@
 <?php
-	/**
-	 * GIT DEPLOYMENT SCRIPT
-	 *
-	 * Used for automatically deploying websites via github or bitbucket, more deets here:
-	 *
-	 *		https://gist.github.com/1809044
-	 */
-	// The commands
-	$commands = 'git pull'
-	// Run the commands for output
-	// foreach($commands AS $command){
-		// Run it
-		$tmp = shell_exec($commands);
-		// Output
-	}
-	// Make it pretty for manual user access (and why not?)
+ // Prevent accidental XSS
+ header("Content-type: text/plain"); 
+ // Run the script - make sure that your SSH key is set with *no* password
+  if ( $_POST['payload'] ) {
+  $tmp = shell_exec("./pull.sh"); 
+  echo "<td>".$tmp."</td>";
+  // $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+  // $output .= htmlentities(trim($tmp)) . "\n";
+ }
 ?>
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-</head>
-<body>
-
-<?php echo $tmp ?>
-</pre>
-</body>
-</html>
